020c23314faafa5681621e054e1f8fa04ad605e9
Wheaties <3laverack@gmail.com>
2021-03-10 22:45:00 -0500

    finished power calculation, started bulk calc

--- dmgcalc/js/ap_calc.js
+++ dmgcalc/js/ap_calc.js
@@ -635,7 +635,7 @@ function calculateAll(rerender_surv_dd = true) {
   var p1 = new Pokemon($("#p1"), field);
   var p2 = new Pokemon($("#p2"), field);
   damageResults = calculateAllMoves(p1, p2, field);
-  powercalc(p1, p2, field);
+  powercalc(p1, p2, field, damageResults);
   var result,
     minDamage,
     maxDamage,
