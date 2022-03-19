c48ff65f5151bea012ebb308ac584fae8eb51492
Ethan Laverack <3laverack@gmail.com>
2022-03-19 18:13:17 -0400

    finalized damage calc feature proposal

--- dmgcalc/js/ap_calc.js
+++ dmgcalc/js/ap_calc.js
@@ -1787,6 +1787,7 @@ $(".result-move").change(function () {
           "</span>"
       );
       $("#mainResultDamage").html(result.damageText);
+      $("#mainResultPower").html(result.powerText);
       $("#mainResultKO").html(result.koChanceText);
       $("#damageValues").text("(" + result.damage.join(", ") + ")");
     }
